e2c733ccc22a53e118d3cda1b21ddf846e07fad9
kylecollie <kcollie@gmail.com>
2016-11-24 20:04:14 -0500

    backeend services done...for now

--- routes/recipes.js
+++ routes/recipes.js
@@ -15,12 +15,87 @@ router.get('/recipes', function (req, res, next) {
 
 // Get single recipe
 router.get('/recipe/:id', function (req, res, next) {
-    db.recipes.findOne({_id: mongojs.ObjectId(req.params.id) }, function (err, recipes) {
+    db.recipes.findOne({
+        _id: mongojs.ObjectId(req.params.id)
+    }, function (err, recipes) {
         if (err) {
             res.send(err);
         }
         res.json(recipes);
     });
 });
+
+// Save new recipe
+router.post('/recipe', function (req, res, next) {
+    var recipe = req.body;
+    if (!recipe.title) {
+        res.status(400);
+        res.json({
+            "error": "Bad data"
+        });
+    } else {
+        db.recipes.save(recipe, function (err, recipe) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(recipes);
+        });
+    }
+});
+
+// Delete a recipe
+router.delete('/recipe/:id', function (req, res, next) {
+    db.recipes.remove({
+        _id: mongojs.ObjectId(req.params.id)
+    }, function (err, recipes) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(recipes);
+    });
+});
+
+// Update existing recipe
+router.put('/recipe/:id', function (req, res, next) {
+    var recipe = req.body;
+    var updRecipe = {};
+
+    if (recipe.title) {
+        updRecipe.title = recipe.title;
+    }
+
+    if (recipe.description) {
+        updRecipe.description = recipe.description;
+    }
+
+    if (recipe.ingredients) {
+        updRecipe.ingredients = recipe.ingredients;
+    }
+
+    if (recipe.instructions) {
+        updRecipe.instructions = recipe.instructions;
+    }
+
+    if (recipe.tags) {
+        updRecipe.tags = recipe.tags;
+    }
+
+    if (!updRecipe) {
+        res.status(400);
+        res.json({
+            "error": "Bad data"
+        });
+    } else {
+        db.recipes.update({
+            _id: mongojs.ObjectId(req.params.id)
+        }, updRecipe, {}, function (err, recipes) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(recipes);
+        });
+    }
+});
+
 
 module.exports = router;
