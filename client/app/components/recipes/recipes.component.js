fbb86a815c373d44e5bce272327e40c874ab8071
kylecollie <kcollie@gmail.com>
2016-11-26 21:45:27 -0500

    mock up done starting to look like an app

--- client/app/components/recipes/recipes.component.js
+++ client/app/components/recipes/recipes.component.js
@@ -11,7 +11,54 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var RecipesComponent = (function () {
     function RecipesComponent() {
-        this.tags = [];
+        this.Recipe = {
+            "_id": "5837315303ac7fb6a44da448",
+            "title": "Smores",
+            "description": "Yummy chocolate covered marshmello treats",
+            "ingredients": [
+                {
+                    "name": "grahm cracker",
+                    "quantity": "2"
+                },
+                {
+                    "name": "chocolate square",
+                    "quantity": "1"
+                },
+                {
+                    "name": "marshmellow",
+                    "quantity": "1"
+                }
+            ],
+            "instructions": [
+                {
+                    "order": 1,
+                    "direction": "place chocolate square on cracker"
+                },
+                {
+                    "order": 2,
+                    "direction": "toast marshmellow"
+                },
+                {
+                    "order": 3,
+                    "direction": "place marshmellow on chocolate"
+                },
+                {
+                    "order": 4,
+                    "direction": "place cracker on top"
+                },
+                {
+                    "order": 5,
+                    "direction": "place cracker on top"
+                }
+            ],
+            "tags": [
+                "chocolate",
+                "marshmellow",
+                "grahm cracker",
+                "campfire",
+                "treat"
+            ]
+        };
     }
     RecipesComponent = __decorate([
         core_1.Component({
--- client/app/components/recipes/recipes.component.ts
+++ client/app/components/recipes/recipes.component.ts
@@ -1,11 +1,58 @@
 import { Component } from '@angular/core';
 
 @Component({
-  moduleId: module.id,
-  selector: 'recipes',
-  templateUrl: 'recipes.component.html'
+    moduleId: module.id,
+    selector: 'recipes',
+    templateUrl: 'recipes.component.html'
 })
 
 export class RecipesComponent {
-  public tags = [];
+    public Recipe = {
+        "_id": "5837315303ac7fb6a44da448",
+        "title": "Smores",
+        "description": "Yummy chocolate covered marshmello treats",
+        "ingredients": [
+            {
+                "name": "grahm cracker",
+                "quantity": "2"
+            },
+            {
+                "name": "chocolate square",
+                "quantity": "1"
+            },
+            {
+                "name": "marshmellow",
+                "quantity": "1"
+            }
+        ],
+        "instructions": [
+            {
+                "order": 1,
+                "direction": "place chocolate square on cracker"
+            },
+            {
+                "order": 2,
+                "direction": "toast marshmellow"
+            },
+            {
+                "order": 3,
+                "direction": "place marshmellow on chocolate"
+            },
+            {
+                "order": 4,
+                "direction": "place cracker on top"
+            },
+            {
+                "order": 5,
+                "direction": "place cracker on top"
+            }
+        ],
+        "tags": [
+            "chocolate",
+            "marshmellow",
+            "grahm cracker",
+            "campfire",
+            "treat"
+        ]
+    }
 }
