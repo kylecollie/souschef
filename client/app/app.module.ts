5f823d2fa2e34e9eb51cc6d81e7bbf0fedfb0271
kylecollie <kcollie@gmail.com>
2016-12-03 15:56:53 -0500

    Provide routing
    Add about page
    Update navbar with working links:
    + /
    + about
    + addRecipe

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
@@ -9,17 +10,34 @@ import { IngredientComponent } from './components/recipes/ingredient.component';
 import { InstructionComponent } from './components/recipes/instruction.component';
 import { TagComponent } from './components/recipes/tag.component';
 import { RecipeFormComponent } from './components/recipes/recipeForm.component';
+import { AboutComponent } from './components/misc/about.component';
 
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule , HttpModule],
+  imports: [
+    BrowserModule, 
+    FormsModule, 
+    ReactiveFormsModule , 
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'addRecipe',
+        component: RecipeFormComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      }
+    ])
+    ],
   declarations: [
     AppComponent, 
     RecipesComponent, 
     RecipeFormComponent,
     IngredientComponent,
     InstructionComponent,
-    TagComponent
+    TagComponent,
+    AboutComponent
   ],
   bootstrap: [AppComponent]
 })
