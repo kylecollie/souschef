fe05d2126f797d060d3624d7a2865739b9156358
kyle <kcollie@hotmail.com>
2016-12-10 03:01:23 -0500

    Add favicon

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
 
 var index = require('./routes/index');
 var recipes = require('./routes/recipes');
@@ -23,14 +24,14 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', index);
 app.use('/api', recipes);
+app.use(favicon(path.join(__dirname,'client', 'app', 'assets', 'images','favicon-32x32.png')));
 
 // serve index.html for all remaining routes, in order to leave routing up to angular
     app.all("/*", function(req, res, next) {
         res.sendFile("index.html", { root: __dirname + "/views" });
     });
 
+
 app.listen(port, function () {
     console.log('Server started on port ' + port);
 });
-
-//32.16
